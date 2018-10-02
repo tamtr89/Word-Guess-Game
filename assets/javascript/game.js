bf912252bd24327b319e23751e5e01bb2673a05f
tam <taam.tr17@gmail.com>
2018-10-02 14:53:59 -0700

    I added reset CSS , trying on html java files.

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+console.log("HI LOve");
+
+// Press anykey to start//
+var button = document.getElementById("press");
+    button.addEventListener("click", myFunction);   
+}
+
+// An array that list of all letter under user's guess//
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z"];
+
+// 
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessRemaining= [];
+
+// Get Element
+var songList = document.getElementById("songs");
+var word;
+var guess;
+var guesses = [];
+var lives;
+var counter;
+var space;
+
+// 
+var button = funtion () {
+    myButton =
+}
